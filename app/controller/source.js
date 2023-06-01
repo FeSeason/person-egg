41759c4830e601bb51e7ac5e3d353ecc5168f47b
Season <season@macbook.local>
2023-06-01 18:46:31 +0800

    feat: 测试cookie

---
+++ app/controller/source.js
@@ -0,0 +1,22 @@
+/* eslint-disable array-bracket-spacing */
+/* eslint-disable array-callback-return */
+'use strict';
+
+const { Controller } = require('egg');
+// const path = require('path');
+// const fs = require('fs');
+
+class SourceController extends Controller {
+
+  async cookie() {
+    const { ctx } = this;
+    const expires = new Date();
+
+    expires.setDate(expires.getDate() + 1); // 过期时间设置为 7 天后
+
+    ctx.cookies.set('myCookie', 'HelloWorld', { domain: 'i.nianxiaonian.com', path: '/', expires });
+    ctx.body = 'Cookie set successfully.';
+  }
+}
+
+module.exports = SourceController;
--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  router.get('/source/pt.js', controller.source.cookie);
   router.post('/upload', controller.home.upload);
   router.post('/submit', controller.home.matchExcel);
 };
